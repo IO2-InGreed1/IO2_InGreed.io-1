a837d8035959ecb834aad8500aa60033a191fc60
portalion <77tobik77@gmail.com>
2024-03-16 20:43:45 +0100

    add model for role and users

---
+++ Src/InGreed.Domain/Enums/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace InGreed.Domain.Enums;
+
+[Flags]
+public enum Role
+{
+    User = 1,
+    Producent = 2,
+    Moderator = 4,
+    Administrator = 8
+}
---
+++ Src/InGreed.Domain/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using InGreed.Domain.Enums;
+
+namespace InGreed.Domain.Models;
+
+public class User
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public Role Role { get; set; } = 0;
+    public bool Banned { get; set; } = false;
+}
