29e7d622fa3fb3e0cf1ba6d1f88fb47211041a14
Zuzanna Wójtowicz <zuzannawojtowicz42@gmail.com>
2024-04-21 12:55:23 +0200

    adjust file structure

--- Src/InGreed.Application/Services/ProductService.cs
+++ Src/InGreed.Application/Services/ProductService.cs
@@ -1,8 +1,42 @@
-﻿
+﻿using InGreed.Application.Interfaces;
 using InGreed.Domain.Models;
 
 namespace InGreed.Application.Services;
 
-public class ProductService
+public class ProductService : IProductService
 {
+    public void AddIngredient(Product product, Ingredient ingredient)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddOpinion(Product product, Opinion opinion)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteOpinion(Product product, Opinion opinion)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product GetProductById(int productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void PromoteProduct(int productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveIngredient(Product product, Ingredient ingredient)
+    {
+        throw new NotImplementedException();
+    }
 }
