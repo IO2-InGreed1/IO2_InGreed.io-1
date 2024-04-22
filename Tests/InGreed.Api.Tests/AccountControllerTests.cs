869de222096d059aa4b1acab5d7fd2736f0d5955
Michal Wozniak <miwoz@eg.dk>
2024-04-22 14:00:50 +0200

    refactor accountcontroller tests

--- Tests/InGreed.Api.Tests/AccountControllerTests.cs
+++ Tests/InGreed.Api.Tests/AccountControllerTests.cs
@@ -1,69 +1,53 @@
 using InGreed.Api.Contracts.Authorization;
 using InGreed.Api.Controllers;
+using InGreed.Api.Mappers;
 using InGreed.Domain.Enums;
 using InGreed.Domain.Models;
 using InGreed.Logic.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System.Net;
 
 namespace InGreed.Api.Tests
 {
-    // TODO: Refactor
     public class AccountControllerTests
     {
-        AccountController _accountController = null!;
-        IAccountService _accountService = null!;
+        User testingUser;
+        string correctToken;
 
-        User existingUser = null!;
-        User nonExistingUser = null!;
-        string correctToken = string.Empty;
-
-        void MockAccountService()
-        {
-            var mock = new Mock<IAccountService>();
-            mock.Setup(accountService => accountService.Login(existingUser)).Returns(correctToken);
-            mock.Setup(accountService => accountService.Login(nonExistingUser)).Throws(new ArgumentException("Invalid Credentials"));
-            mock.Setup(accountService => accountService.Register(existingUser)).Throws(new ArgumentException("User already registered"));
-            mock.Setup(accountService => accountService.Register(nonExistingUser)).Returns(correctToken);
-
-            _accountService = mock.Object;
-        }
+        Mock<IAccountService> accountServiceMock;
+        Mock<IContractsToModelsMapper> contractsToModelsMapperMock;
 
         public AccountControllerTests()
         {
-            existingUser = new User
+            testingUser = new User
             {
                 Id = 1,
                 Banned = false,
-                Email = "Existing@test.com",
-                Password = "Existing",
-                Username = "ExistingTest",
-                Role = Role.User
-            };
-            nonExistingUser = new User
-            {
-                Banned = false,
-                Email = "NonExisting@test.com",
-                Password = "NonExisting",
-                Username = "NonExistingTest",
+                Email = "test@test.com",
+                Password = "test",
+                Username = "test",
                 Role = Role.User
             };
 
-            correctToken = "correctToken";
+            correctToken = "Correct Token";
 
-            MockAccountService();
-
-            _accountController = new AccountController(_accountService);
+            accountServiceMock = new Mock<IAccountService>();
+            contractsToModelsMapperMock = new Mock<IContractsToModelsMapper>();
         }
 
         [Fact]
         public void UserLogin_CorrectCredentials_ShouldReturnStatusOk()
         {
-            var request = new LoginRequest(existingUser.Email, existingUser.Password);
+            //Arrange
+            var request = new LoginRequest(testingUser.Email, testingUser.Password);
+            contractsToModelsMapperMock.Setup(ctm => ctm.LoginRequestToUser(request)).Returns(testingUser);
+            accountServiceMock.Setup(ac => ac.Login(testingUser)).Returns(correctToken);
+            var accountController = new AccountController(accountServiceMock.Object, contractsToModelsMapperMock.Object);
 
-            var response = _accountController.Login(request);
+            //Act
+            var response = accountController.Login(request);
 
+            //Assert
             var actionResult = Assert.IsType<OkObjectResult>(response);
             var responseContent = Assert.IsType<AuthorizationResponse>(actionResult.Value);
             Assert.NotNull(responseContent.authorizationToken);
@@ -73,20 +57,32 @@ namespace InGreed.Api.Tests
         [Fact]
         public void UserLogin_IncorrectCredentials_ShouldReturnStatusForbidden()
         {
-            var request = new LoginRequest(nonExistingUser.Email, nonExistingUser.Password);
+            //Arrange
+            var request = new LoginRequest(testingUser.Email, testingUser.Password);
+            contractsToModelsMapperMock.Setup(ctm => ctm.LoginRequestToUser(request)).Returns(testingUser);
+            accountServiceMock.Setup(ac => ac.Login(testingUser)).Throws(new ArgumentException("Invalid Credentials"));
+            var accountController = new AccountController(accountServiceMock.Object, contractsToModelsMapperMock.Object);
 
-            var response = _accountController.Login(request);
+            //Act
+            var response = accountController.Login(request);
 
+            //Assert
             Assert.IsType<ForbidResult>(response);
         }
 
         [Fact]
         public void UserRegister_CorrectUser_ShouldReturnStatusOk()
         {
-            var request = new RegisterRequest(nonExistingUser.Email, nonExistingUser.Username, nonExistingUser.Password);
+            //Arrange
+            var request = new RegisterRequest(testingUser.Email, testingUser.Username, testingUser.Password);
+            contractsToModelsMapperMock.Setup(ctm => ctm.RegisterRequestToUser(request)).Returns(testingUser);
+            accountServiceMock.Setup(ac => ac.Register(testingUser)).Returns(correctToken);
+            var accountController = new AccountController(accountServiceMock.Object, contractsToModelsMapperMock.Object);
 
-            var response = _accountController.Register(request);
+            //Act
+            var response = accountController.Register(request);
 
+            //Assert
             var actionResult = Assert.IsType<OkObjectResult>(response);
             var responseContent = Assert.IsType<AuthorizationResponse>(actionResult.Value);
             Assert.NotNull(responseContent.authorizationToken);
@@ -94,22 +90,18 @@ namespace InGreed.Api.Tests
         }
 
         [Fact]
-        public void UserRegister_ExistingUser_ShouldReturnStatusConflict()
-        {
-            var request = new RegisterRequest(existingUser.Email, existingUser.Username, existingUser.Password);
-
-            var response = _accountController.Register(request);
-
-            Assert.IsType<ConflictResult>(response);
-        }
-
-        [Fact]
         public void UserRegister_IncorrectCredentials_ShouldReturnStatusBadRequest()
         {
-            var request = new RegisterRequest(nonExistingUser.Email, nonExistingUser.Username, existingUser.Password);
+            //Arrange
+            var request = new RegisterRequest(testingUser.Email, testingUser.Username, testingUser.Password);
+            contractsToModelsMapperMock.Setup(ctm => ctm.RegisterRequestToUser(request)).Returns(testingUser);
+            accountServiceMock.Setup(ac => ac.Register(testingUser)).Throws(new ArgumentException("User already registered"));
+            var accountController = new AccountController(accountServiceMock.Object, contractsToModelsMapperMock.Object);
 
-            var response = _accountController.Register(request);
+            //Act
+            var response = accountController.Register(request);
 
+            //Assert
             Assert.IsType<BadRequestResult>(response);
         }
     }
