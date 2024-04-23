d0a547602af70a586a2c0c05e5dc8e99a299a66c
Portalion <77tobik77@gmail.com>
2024-04-23 21:09:27 +0200

    add missing files

---
+++ Src/InGreed.Logic/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using InGreed.DataAccess.Interfaces;
+using InGreed.Logic.Interfaces;
+using InGreed.Domain.Models;
+
+namespace InGreed.Logic.Services;
+
+public class ProductService : IProductService
+{
+    IProductDA _productDA;
+    public ProductService(IProductDA productDA)
+    {
+        _productDA = productDA;
+    }
+
+    public int CreateProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product GetProductById(int productId)
+    {
+        throw new NotImplementedException();
+    }
+}
