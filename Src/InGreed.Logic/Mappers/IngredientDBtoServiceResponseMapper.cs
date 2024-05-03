f12e1a57c5074551394287680361987648ffc4b5
Zuzanna Wójtowicz <137983594+zuzwoj@users.noreply.github.com>
2024-05-03 12:05:07 +0200

    Zuzia/ingredient service (#23)
    * add initial file structure + some WIP code
    
    * add IIngredientDA interface
    
    * write down unit test ideas + minor fixes
    
    * fix suggested by Kuba
    
    * begin adding controller function headers
    
    * remove GetByProduct methods in Ingredient related classes
    
    * begin the test implementation
    
    * finish ingredient service related implementation aside from adding to and removing from products
    
    * minor fix before pr
    
    * add missing tests in ServiceControllerTests
    
    * add empty contract files
    
    * add contracts
    
    * use contracts in IngredientController
    
    * use contracts in controller tests
    
    * add IngredientServiceResponse enums to retreive information about addition and removal of ingredients and implement missing IngredientControllerTests
    
    * finalize controllers
    
    * return status in product data access + fix some typos
    
    * implement missing functions
    
    * portalion fix

---
+++ Src/InGreed.Logic/Mappers/IngredientDBtoServiceResponseMapper.cs
@@ -0,0 +1,12 @@
+﻿using InGreed.DataAccess.Enums;
+using InGreed.Logic.Enums;
+using Riok.Mapperly.Abstractions;
+
+namespace InGreed.Logic.Mappers;
+
+[Mapper(EnumMappingStrategy = EnumMappingStrategy.ByName, EnumMappingIgnoreCase = true)]
+public partial class IngredientDBtoServiceResponseMapper : IIngredientDBtoServiceResponseMapper
+{
+    public partial IngredientServiceAddResponse AddResponseMapper(IngredientDAAddResponse DAresponse);
+    public partial IngredientServiceRemoveResponse RemoveResponseMapper(IngredientDARemoveResponse DAresponse);
+}
---
+++ Tests/InGreed.Logic.Test/IngredientServiceTests.cs
@@ -0,0 +1,207 @@
+﻿using InGreed.DataAccess.Interfaces;
+using InGreed.Logic.Services;
+using InGreed.Domain.Models;
+using Moq;
+using InGreed.Logic.Enums;
+using InGreed.DataAccess.Enums;
+using InGreed.Logic.Mappers;
+
+namespace InGreed.Logic.Tests;
+
+public class IngredientServiceTests
+{
+    private Ingredient testingIngredient;
+    private Mock<IIngredientDA> ingredientDAMock;
+    private IngredientDBtoServiceResponseMapper mapper;
+    private readonly int id = 1;
+    private readonly int badId = 2;
+
+    public IngredientServiceTests() 
+    {
+        ingredientDAMock = new();
+        testingIngredient = new() { Id = id, Name = "test", IconURL = string.Empty };
+        mapper = new();
+    }
+
+    [Fact]
+    public void GetAll_ShouldReturnAllIngredients() 
+    {
+        // Arrange
+        List<Ingredient> ingredients = new() { testingIngredient };
+        ingredientDAMock.Setup(ida => ida.GetAll()).Returns(ingredients);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.GetAll();
+
+        // Assert
+        Assert.Equal(result, ingredients);
+    }
+
+    [Fact]
+    public void GetById_Existingingredient_ShouldReturnCorrectIngredient() 
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(testingIngredient);
+        ingredientDAMock.Setup(ida => ida.GetById(badId)).Returns(value: null);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.GetById(id);
+
+        // Assert
+        Assert.Equal(result, testingIngredient);
+    }
+
+    [Fact]
+    public void GetById_NonexistentIngredient_ShouldReturnNull() 
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(testingIngredient);
+        ingredientDAMock.Setup(ida => ida.GetById(badId)).Returns(value: null);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.GetById(badId);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void AddToProduct_NonexistentIngredientExistingProduct_ShouldReturnSuccessResponse()
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.Create(testingIngredient)).Returns(id);
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(value: null);
+        ingredientDAMock.Setup(ida => ida.AddToProduct(id, id)).Returns(IngredientDAAddResponse.Success);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.AddToProduct(testingIngredient, id);
+
+        // Assert
+        Assert.Equal(IngredientServiceAddResponse.Success, result);
+    }
+
+    [Fact]
+    public void AddToProduct_NonexistentIngredientNonexistentProduct_ShouldReturnNonexistentProductResponse()
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.Create(testingIngredient)).Returns(id);
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(value: null);
+        ingredientDAMock.Setup(ida => ida.AddToProduct(id, id)).Returns(IngredientDAAddResponse.NonexistentProduct);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.AddToProduct(testingIngredient, id);
+
+        // Assert
+        Assert.Equal(IngredientServiceAddResponse.NonexistentProduct, result);
+    }
+
+    [Fact]
+    public void AddToProduct_ExistingIngredientExistingProduct_ShouldReturnSuccessResponse()
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(testingIngredient);
+        ingredientDAMock.Setup(ida => ida.AddToProduct(id, id)).Returns(IngredientDAAddResponse.Success);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.AddToProduct(testingIngredient, id);
+
+        // Assert
+        Assert.Equal(IngredientServiceAddResponse.Success, result);
+    }
+
+    [Fact]
+    public void AddToProduct_ExistingIngredientNonexistentProduct_ShouldReturnNonexistentProductResponse()
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(testingIngredient);
+        ingredientDAMock.Setup(ida => ida.AddToProduct(id, id)).Returns(IngredientDAAddResponse.NonexistentProduct);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.AddToProduct(testingIngredient, id);
+
+        // Assert
+        Assert.Equal(IngredientServiceAddResponse.NonexistentProduct, result);
+    }
+
+    [Fact]
+    public void RemoveFromProduct_NonexistentIngredientExistingProduct_ShouldReturnIngredientNotFromProductResponse()
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(value: null);
+        ingredientDAMock.Setup(ida => ida.RemoveFromProduct(id, id)).Returns(IngredientDARemoveResponse.IngredientNotFromProduct);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.RemoveFromProduct(id, id);
+
+        // Assert
+        Assert.Equal(IngredientServiceRemoveResponse.IngredientNotFromProduct, result);
+    }
+
+    [Fact]
+    public void RemoveFromProduct_NonexistentIngredientNonexistentProduct_ShouldReturnNonexistentProductResponse()
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(value: null);
+        ingredientDAMock.Setup(ida => ida.RemoveFromProduct(id, id)).Returns(IngredientDARemoveResponse.NonexistentProduct);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.RemoveFromProduct(id, id);
+
+        // Assert
+        Assert.Equal(IngredientServiceRemoveResponse.NonexistentProduct, result);
+    }
+
+    [Fact]
+    public void RemoveFromProduct_ExistingIngredientFromTheProduct_ShouldReturnSuccessResponse()
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(testingIngredient);
+        ingredientDAMock.Setup(ida => ida.RemoveFromProduct(id, id)).Returns(IngredientDARemoveResponse.Success);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.RemoveFromProduct(id, id);
+
+        // Assert
+        Assert.Equal(IngredientServiceRemoveResponse.Success, result);
+    }
+
+    [Fact]
+    public void RemoveFromProduct_ExistingIngredientNotFromTheProduct_ShouldReturnIngredientNotFromProductResponse()
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(testingIngredient);
+        ingredientDAMock.Setup(ida => ida.RemoveFromProduct(id, id)).Returns(IngredientDARemoveResponse.IngredientNotFromProduct);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.RemoveFromProduct(id, id);
+
+        // Assert
+        Assert.Equal(IngredientServiceRemoveResponse.IngredientNotFromProduct, result);
+    }
+
+    [Fact]
+    public void RemoveFromProduct_ExistingIngredientNonexistentProduct_ShouldReturnNonexistentProductResponse()
+    {
+        // Arrange
+        ingredientDAMock.Setup(ida => ida.GetById(id)).Returns(testingIngredient);
+        ingredientDAMock.Setup(ida => ida.RemoveFromProduct(id, id)).Returns(IngredientDARemoveResponse.NonexistentProduct);
+        IngredientService sut = new(ingredientDAMock.Object, mapper);
+
+        // Act
+        var result = sut.RemoveFromProduct(id, id);
+
+        // Assert
+        Assert.Equal(IngredientServiceRemoveResponse.NonexistentProduct, result);
+    }
+}
