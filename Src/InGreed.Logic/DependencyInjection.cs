5817ae2095f24e3696bfaa0352ddbfb055625ae0
Zuzanna Wójtowicz <137983594+zuzwoj@users.noreply.github.com>
2024-05-05 17:37:33 +0200

    fix dependency injection (#24)
    * add services to Program.cs
    
    * use DependencyInjection like a normal person would

--- Src/InGreed.Logic/DependencyInjection.cs
+++ Src/InGreed.Logic/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using InGreed.Logic.Interfaces;
+using InGreed.Logic.Mappers;
 using InGreed.Logic.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +11,9 @@ public static class DependencyInjection
     {
         return services
             .AddScoped<IAccountService, AccountService>()
-            .AddScoped<ITokenService, FakeTokenService>();
+            .AddScoped<ITokenService, FakeTokenService>()
+            .AddScoped<IIngredientService, IngredientService>()
+            .AddScoped<IProductService, ProductService>()
+            .AddScoped<IIngredientDBtoServiceResponseMapper, IngredientDBtoServiceResponseMapper>();
     }
 }
