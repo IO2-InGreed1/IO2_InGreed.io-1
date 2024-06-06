6bc6a2cf4cc5781cca614bbfa7c3e1a2a4fb19b0
Zuzanna Wójtowicz <137983594+zuzwoj@users.noreply.github.com>
2024-06-06 18:12:47 +0200

    Preference model hotfix (#32)

--- Src/InGreed.Domain/Models/Preference.cs
+++ Src/InGreed.Domain/Models/Preference.cs
@@ -7,8 +7,8 @@ public class Preference
     public int Id { get; set; }
     public int OwnerId { get; set; }
     public string Name { get; set; } = string.Empty;
-    public HashSet<Ingredient> Forbidden { get; } = new();
-    public HashSet<Ingredient> Preferred { get; } = new();
+    public HashSet<Ingredient> Forbidden { get; set; } = new();
+    public HashSet<Ingredient> Preferred { get; set; } = new();
     public Category? Category { get; set; } = null;
     public bool Active { get; set; } = new();
 }
