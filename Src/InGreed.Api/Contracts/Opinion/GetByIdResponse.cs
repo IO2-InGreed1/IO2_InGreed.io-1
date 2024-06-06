08196d314f03f78003d824b04c17fa5d7088e3fd
Jakub Wierzbowski <wierzbos31@gmail.com>
2024-06-06 20:33:09 +0200

    initial Opinion controller and contracts

---
+++ Src/InGreed.Api/Contracts/Opinion/GetByIdResponse.cs
@@ -0,0 +1,3 @@
+namespace InGreed.Api.Contracts.Opinion;
+
+public record GetByIdResponse(Domain.Models.Opinion opinion);
--- Src/InGreed.Api/Controllers/OpinionController.cs
+++ Src/InGreed.Api/Controllers/OpinionController.cs
@@ -1,7 +1,9 @@
-using InGreed.Api.Mappers;
+using InGreed.Api.Contracts.Opinion;
+using InGreed.Domain.Models;
 using InGreed.Logic.Interfaces;
-using InGreed.Logic.Services;
 using Microsoft.AspNetCore.Mvc;
+using InGreed.Logic.Enums.Opinion;
+
 
 namespace InGreed.Api.Controllers;
 
@@ -14,5 +16,46 @@ public class OpinionController : ControllerBase
     public OpinionController(IOpinionService opinionService)
     {
         _opinionService = opinionService;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        Opinion? result = _opinionService.GetById(id);
+        if (result is null) return BadRequest();
+        GetByIdResponse response = new(result);
+        return Ok(response);
+    }
+
+    [HttpPost("/api/Product/{productId}/add-opinion")]
+    public IActionResult AddToProduct(AdditionRequest request, int productId)
+    {
+        OpinionServiceAddResponse result = _opinionService.AddToProduct(request.opinion, productId);
+        switch (result)
+        {
+            case OpinionServiceAddResponse.NonexistentProduct:
+                return NotFound($"There is no product with an id {productId}.");
+            case OpinionServiceAddResponse.Success:
+                return Ok();
+            default:
+                return BadRequest("Unexpected error.");
+        }
+    }
+
+    [HttpDelete("/api/Product/{productId}/remove-opinion/{opinionId}")]
+    public IActionResult RemoveFromProduct(int opinionId, int productId)
+    {
+        OpinionServiceRemoveResponse result = _opinionService.RemoveFromProduct(opinionId, productId);
+        switch (result)
+        {
+            case OpinionServiceRemoveResponse.Success:
+                return Ok();
+            case OpinionServiceRemoveResponse.IngredientNotFromProduct:
+                return NotFound($"Product {productId} does not contain an Opinion with an id {opinionId}.");
+            case OpinionServiceRemoveResponse.NonexistentProduct:
+                return NotFound($"There is no product with an id {productId}.");
+            default:
+                return BadRequest("Unexpected error.");
+        }
     }
 }
