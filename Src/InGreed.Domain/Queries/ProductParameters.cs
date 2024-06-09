0193f7377b6725c7aad9c7a0a9d55bc7d94d4185
Zuzanna Wójtowicz <137983594+zuzwoj@users.noreply.github.com>
2024-06-09 10:49:32 +0200

    product name in filters + fix product modification (#51)

--- Src/InGreed.Domain/Queries/ProductParameters.cs
+++ Src/InGreed.Domain/Queries/ProductParameters.cs
@@ -5,6 +5,7 @@ namespace InGreed.Domain.Queries;
 public class ProductParameters : PaginationParameters
 {
     public Category? Category { get; set; } = null;
+    public string? Name { get; set; } = null;
     public List<int> usedIngredientsId { get; set; } = new List<int>();
     public List<int> bannedIngredientsId { get; set;} = new List<int>();
 }
